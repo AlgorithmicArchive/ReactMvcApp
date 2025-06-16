020771bd76d05208b37140e050189f927ab37d96
Momin Rather <algorithmicarchive@gmail.com>
2025-06-16 16:54:39 +0530

    Minor Changes

--- src/components/form/DynamicStepForm.jsx
+++ src/components/form/DynamicStepForm.jsx
@@ -113,195 +113,6 @@ const DynamicStepForm = ({ mode = "new", data }) => {
   const [bankNameBlurred, setBankNameBlured] = useState(false);
   const [branchNameBlurred, setBranchNameBlured] = useState(false);
 
-  const dummyData = {
-    Location: [
-      {
-        label: "Select District",
-        name: "District",
-        value: 5,
-      },
-      {
-        label: "Select Tehsil Social Welfare Office (TSWO)",
-        name: "Tehsil",
-        value: 79,
-      },
-    ],
-    "Pension Type": [
-      {
-        label: "Pension Type",
-        name: "PensionType",
-        value: "OLD AGE PENSION",
-      },
-    ],
-    "Applicant Details": [
-      {
-        label: "Applicant Name",
-        name: "ApplicantName",
-        value: "RHAUL SHARMA",
-      },
-      {
-        label: "Applicant Image",
-        name: "ApplicantImage",
-        File: "/uploads/bf279f20.jpg",
-      },
-      {
-        label: "Date of Birth",
-        name: "DateOfBirth",
-        value: "1960-02-02",
-      },
-      {
-        label: "Mobile Number",
-        name: "MobileNumber",
-        value: "9237283728",
-      },
-      {
-        label: "Email",
-        name: "Email",
-        value: "randomizerweb129@gmail.com",
-      },
-      {
-        label: "Category",
-        name: "Category",
-        value: "AYY",
-      },
-      {
-        label: "Relation",
-        name: "Relation",
-        value: "Father",
-      },
-      {
-        label: "Relation Name",
-        name: "RelationName",
-        value: "SHOBIT SHARMA",
-      },
-      {
-        label: "Gender",
-        name: "Gender",
-        value: "Male",
-      },
-    ],
-    "Present Address Details": [
-      {
-        label: "Address",
-        name: "PresentAddress",
-        value: "16 TRIKUTA NAGAR",
-      },
-      {
-        label: "District",
-        name: "PresentDistrict",
-        value: 5,
-      },
-      {
-        label: "Tehsil",
-        name: "PresentTehsil",
-        value: 79,
-      },
-      {
-        label: "Block",
-        name: "PresentBlock",
-        value: "BLOCK",
-      },
-      {
-        label: "Halqa Panchayat / Municipality Name",
-        name: "PresentHalqaPanchayatMunicipalityName",
-        value: "PANCHAYAT",
-      },
-      {
-        label: "Village",
-        name: "PresentVillage",
-        value: "VILLAGE",
-      },
-      {
-        label: "Ward",
-        name: "PresentWard",
-        value: "WARD",
-      },
-      {
-        label: "Pincode",
-        name: "PresentPincode",
-        value: "180001",
-      },
-    ],
-    "Permanent Address Details": [
-      {
-        label: "Address",
-        name: "PermanentAddress",
-        value: "16 TRIKUTA NAGAR",
-      },
-      {
-        label: "District",
-        name: "PermanentDistrict",
-        value: 5,
-      },
-      {
-        label: "Tehsil",
-        name: "PermanentTehsil",
-        value: 79,
-      },
-      {
-        label: "Block",
-        name: "PermanentBlock",
-        value: "BLOCK",
-      },
-      {
-        label: "Halqa Panchayat / Municipality Name ",
-        name: "PermanentHalqaPanchayatMunicipalityName",
-        value: "PANCHAYAT",
-      },
-      {
-        label: "Village",
-        name: "PermanentVillage",
-        value: "VILLAGE",
-      },
-      {
-        label: "Ward",
-        name: "PermanentWard",
-        value: "WARD",
-      },
-      {
-        label: "Pincode",
-        name: "PermanentPincode",
-        value: "180001",
-      },
-    ],
-    "Bank Details": [
-      {
-        label: "Select Bank",
-        name: "BankName",
-        value: "JAMMU AND KASHMIR BANK",
-      },
-      {
-        label: "Branch Name",
-        name: "BranchName",
-        value: "RESIDENCY ROAD",
-      },
-      {
-        label: "IFSC Code",
-        name: "IfscCode",
-        value: "JAKA0KEEPER",
-      },
-      {
-        label: "Account Number",
-        name: "AccountNumber",
-        value: "1800019292928833",
-      },
-    ],
-    Documents: [
-      {
-        label: "Identity Proof",
-        name: "IdentityProof",
-        Enclosure: "Adhaar Card",
-        File: "/uploads/f42b76d1.pdf",
-      },
-      {
-        label: "Age Proof",
-        name: "AgeProof",
-        Enclosure: "Class 10th Ceritificate",
-        File: "/uploads/42fb28c3.pdf",
-      },
-    ],
-  };
-
   const hasRunRef = useRef(false);
 
   const isFieldDisabled = (fieldName) => {
@@ -400,18 +211,21 @@ const DynamicStepForm = ({ mode = "new", data }) => {
           setInitialData(data);
           reset(data);
         }
-        Object.keys(dummyData).forEach((key) => {
-          dummyData[key].map((item, sectionIndex) => {
-            setValue(item.name, item.value);
-            if (item.name.toLowerCase().includes("district")) {
-              handleDistrictChange(
-                sectionIndex,
-                { name: item.name },
-                item.value
-              );
-            }
+
+        if (data != null) {
+          Object.keys(data).forEach((key) => {
+            data[key].map((item, sectionIndex) => {
+              setValue(item.name, item.value);
+              if (item.name.toLowerCase().includes("district")) {
+                handleDistrictChange(
+                  sectionIndex,
+                  { name: item.name },
+                  item.value
+                );
+              }
+            });
           });
-        });
+        }
       } catch (error) {
         console.error("Error fetching service content:", error);
       } finally {
